0d742f4b93c0208c449a07dccae1fb87a5216099
viktoriakachanenko <vika.danileyko96@gmail.com>
2025-10-21 13:09:00 +0300

    start

---
+++ src/main.tsx
@@ -0,0 +1,12 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import { RouterProvider } from "react-router/dom";
+import { router } from './routes/routes';
+
+import 'src/styles/global.scss'
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+  <RouterProvider router={router} />
+  </StrictMode>
+)
---
+++ src/routes/routes.ts
@@ -0,0 +1,13 @@
+import { createBrowserRouter } from "react-router";
+import { Layout } from "src/components/layout";
+import { Home } from "src/pages/home"
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    Component: Layout,
+    children: [
+      { index: true, Component: Home },
+    ]
+  },
+]);
