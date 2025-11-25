6f3dd6f0eea1733a9662b663f433fddbb87c97bf
viktoriakachanenko <vika.danileyko96@gmail.com>
2025-11-25 14:17:49 +0200

    Table component

--- src/pages/users/users.tsx
+++ src/pages/users/users.tsx
@@ -1,11 +1,14 @@
 import { Counter } from 'src/components/counter';
 import { Filters } from 'src/components/filters';
+import { Table } from 'src/components/table';
+import { columns, users } from 'src/components/table/table';
 
 export const Users = () => {
   return (
     <>
       <Counter people={12} departmens={5} guests={0} />
       <Filters />
+      <Table data={users} columns={columns} />
     </>
   );
 };
